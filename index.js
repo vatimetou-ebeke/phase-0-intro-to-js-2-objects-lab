bba56dc0fa921e2055fdf0a822a98ce73ea01198
vatimetou ebeker <fatimaabcar2@gmail.com>
2021-12-30 17:25:17 -0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+const employee = {
+    name:"jamie",
+
+    streetAddress:{
+        position:"24 ,gelnwood",
+        code:"70002",
+        city:"alexadria"
+
+    },
+
+};
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+     
+    const newEmployee={...employee};
+    newEmployee[key]=value
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+employee[key]=value;
+return employee;    
+
+};
+function deleteFromEmployeeByKey(employee,key){
+
+    const nwemployee={...employee};
+    delete nwemployee.key;
+
+    return nwemployee;
+};
+
+ 
